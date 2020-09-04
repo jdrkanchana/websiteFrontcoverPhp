79f03186c161ab128c77fc4fc2dc9faac7263010
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-04 05:32:54 +0530

    Create template.php

---
+++ template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $title; ?></title>
+
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="banner">
+			</div>
+
+			<nav id="navigation">
+				<ul id="nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Tea</a></li>
+					<li><a href="#">Biscuits</a></li>
+					<li><a href="#">About</a></li>
+					
+				</ul>
+			</nav>
+
+			<div id="content_area">
+				<?php echo $content; ?>
+			</div>
+
+			<div id="sidebar">
+
+			</div>
+
+			<footer>
+				<p>Designed by Dulangi Kanchana 2020</p>
+			</footer>
+		</div>
+	</body>
+</html>
