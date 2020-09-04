be9ac38390050f56f5a9206e6f9a44c81cc2f6ff
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-04 08:19:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
+
 <?php
 $title = "Home";
-$content = "Hello World";
+$content = "
 
-include 'Template.php';
+<p>
+	Come to a soothing new restaurant built just around the corner of Wijerama mawatha,
+	Enjoy the hidden calm, serene environment with palm trees surroounding in the mini forest 
+	around the busy Colombo city. Sit back and enjoy a variety of tea flavours from strawberry tea to flavours your tastebuds are demanding
+	with ozzing hot chocolate waffles and a large mix of variety of goodies in our unique menu..
+</p>
+";
+
+include 'template.php';
 
 
 ?>
